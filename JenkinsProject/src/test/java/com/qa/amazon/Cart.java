2cfc7dca70d956c53d86c8d2a687881caeeb51b3
anup mistry <anupmistry4@gmail.com>
2019-05-07 22:50:56 +0530

    :wq git status
    Changes not staged for commit:
    wq
    :wq
    :wq
    git staqtus
    i
    :WQ

---
+++ JenkinsProject/src/test/java/com/qa/amazon/Cart.java
@@ -0,0 +1,26 @@
+package com.qa.amazon;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Cart {
+	 WebDriver driver;
+	
+	@Test
+	public void invokeBrowser()
+	{
+		System.setProperty("webdriver.chrome.driver","E:\\chromeDriver\\chromedriver.exe");
+		driver=new ChromeDriver();
+		
+		//he
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.get("https://www.amazon.in/");
+
+}
+}
